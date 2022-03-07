a1bc56d44ed681e6f3227244bd61f27dbf4522fc
Krishna <sthakrishnakumar@gmail.com>
2022-03-07 16:27:36 +0545

    rest api flutter

---
+++ android/app/src/main/kotlin/com/example/restapi_flutter/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.restapi_flutter
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+
+void main() {
+  runApp(ProviderScope(child: const MyApp()));
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: const HomePage(),
+    );
+  }
+}
